c25aa14b29b22ad4f788d91db382eeeb2a5e61e1
Mohit ahlawat <65100859+mohitahlawat2001@users.noreply.github.com>
2021-07-04 11:40:29 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+const socket = new WebSocket('ws://localhost:8080');
+
+socket.addEventListener('open', function (event) {
+    socket.send('Hello Server!');
+});
+
+function setup() {
+	createCanvas(1520, 680);
+  background(225);
+  WebSocket
+}
+
+var [xpos, ypos, xspeed, yspeed] = [225, 225, 0, 0];
+
+function draw() {
+  strokeWeight(10)
+	fill(0, 30, 0);
+  if(mouseIsPressed){
+	  line(mouseX, mouseY, pmouseX, pmouseY);
+  }
+}
